49ddf7585bfc36b1eb9c0541807e203c85880fa6
johncrosscross <108315893+johncrosscross@users.noreply.github.com>
2022-10-27 19:51:48 -0400

    Add files via upload

---
+++ Jumping game/script.js
@@ -0,0 +1,29 @@
+
+const character = document.getElementById('character');
+const block = document.getElementById('block');
+
+function jump () {
+	if (character.classList != 'animate') {
+	character.classList.add('animate');
+	}
+	setTimeout (function () {
+		character.classList.remove('animate');
+	}, 500);
+}
+
+function start() {
+	block.classList.add('start');
+}
+
+const checkDead = setInterval (function () {
+	const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+	const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+	if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+		block.style.animation = 'none';
+		block.style.display = 'none';
+		alert('You have lost :(');
+		location.reload();
+		}
+}, 10); 
+
+
